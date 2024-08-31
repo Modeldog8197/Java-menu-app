42d5f0ca6ad48f7a1401b46d6185f4daac6a2f6a
Avdhoot <avdhootgupta0@gmail.com>
2024-08-31 12:15:56 +0530

    Add files via upload

--- menuapp.java
+++ menuapp.java
@@ -26,37 +26,47 @@ public class menuapp {
 
     public static void main(String[] args) {
         Scanner ip = new Scanner(System.in);
-        System.out.println("Which shape would you like to find the area of?");
-        System.out.println("enter 1 for square");
-        System.out.println("enter 2 for circle");
-        System.out.println("enter 3 for rectangle");
-        System.out.println("enter 4 for triangle");
-        int opt = ip.nextInt();
-        switch (opt) {// TODO Auto-generated method stub
-            case 1:
-                System.out.println("enter the side length");
-                double a = ip.nextInt();
-                arsquare(a);
-                break;
-            case 2:
-                System.out.println("enter the radius");
-                double b = ip.nextInt();
-                arcircle(b);
-                break;
-            case 3:
-                System.out.println("enter length and breadth");
-                double c = ip.nextInt();
-                double d = ip.nextInt();
-                arrectangle(c, d);
-                break;
-            case 4:
-                System.out.println("enter the height and base");
-                double e = ip.nextInt();
-                double f = ip.nextInt();
-                artriangle(e, f);
-            default:
-                System.out.println("Please enter a correct value");
-        }
+        char cont = 'y';
+        do{
+            System.out.println("Which shape would you like to find the area of?");
+            System.out.println("enter 1 for square");
+            System.out.println("enter 2 for circle");
+            System.out.println("enter 3 for rectangle");
+            System.out.println("enter 4 for triangle");
+            int opt = ip.nextInt();
+            switch (opt) {
+                case 1:
+                    System.out.println("enter the side length");
+                    double a = ip.nextInt();
+                    arsquare(a);
+                    break;
+                case 2:
+                    System.out.println("enter the radius");
+                    double b = ip.nextInt();
+                    arcircle(b);
+                    break;
+                case 3:
+                    System.out.println("enter length and breadth");
+                    double c = ip.nextInt();
+                    double d = ip.nextInt();
+                    arrectangle(c, d);
+                    break;
+                case 4:
+                    System.out.println("enter the height and base");
+                    double e = ip.nextInt();
+                    double f = ip.nextInt();
+                    artriangle(e, f);
+                    break;
+                default:
+                    System.out.println("Please enter a correct value");
+
+            }
+            System.out.println("Continue? (y/n)"); 
+            cont = ip.next().toLowerCase().charAt(0); 
+            ip.nextLine();
+        }while(cont == 'y');
+        System.out.println("Exiting Program");
+        ip.close();
 
     }
 
